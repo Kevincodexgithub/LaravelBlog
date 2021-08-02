b507910ff3ccd8a26201f76c41f30fba33bd6f6d
Kevincodexgithub <kevintauro95@gmail.com>
2021-08-01 23:24:16 -0500

    controladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,30 +14,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return "Bienvenido a la pagina principal";
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return "Bienvenido a la pagina principal";
+// });
 
-Route::get('cursos', function () {
-    return "Bienvenido a la pagina cursos";
-});
+Route::get('/', HomeController::class);
 
-Route::get('cursos/create', function () {
-    return "En esta pagina podras crear un curso";
-});
+// Route::get('cursos', function () {
+//     return "Bienvenido a la pagina cursos";
+// });
+
+Route::get('cursos', [CursoController::class, 'index']);
+
+// Route::get('cursos/create', function () {
+//     return "En esta pagina podras crear un curso";
+// });
+Route::get('cursos/create', [CursoController::class, 'create']);
 
 // Route::get('cursos/{curso}', function ($curso) {
 //     return "Bienvenido al curso " . $curso;
 // });
 
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
+
 /**
  * TODO: El simbolo (?) despues de la variable significa que la variable es opcional al pasarle por la URL
  */
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
-    if ($categoria) {
-        return "Bienvenido al curso $curso, de la categoria $categoria";
-    } else {
-        return "Bienvenido al curso " . $curso;
-    }
-});
+// Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+//     if ($categoria) {
+//         return "Bienvenido al curso $curso, de la categoria $categoria";
+//     } else {
+//         return "Bienvenido al curso " . $curso;
+//     }
+// });
