634b43e31087b27d22dafc2ead4b028dce7ac66d
Kevincodexgithub <kevintauro95@gmail.com>
2021-08-01 22:31:03 -0500

    rutas

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    return "Bienvenido a la pagina principal";
+});
+
+Route::get('cursos', function () {
+    return "Bienvenido a la pagina cursos";
+});
+
+Route::get('cursos/create', function () {
+    return "En esta pagina podras crear un curso";
+});
+
+// Route::get('cursos/{curso}', function ($curso) {
+//     return "Bienvenido al curso " . $curso;
+// });
+
+/**
+ * TODO: El simbolo (?) despues de la variable significa que la variable es opcional al pasarle por la URL
+ */
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if ($categoria) {
+        return "Bienvenido al curso $curso, de la categoria $categoria";
+    } else {
+        return "Bienvenido al curso " . $curso;
+    }
+});
