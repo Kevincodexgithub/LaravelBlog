f1f4a76d51ce0f175583790cbb6a9fff8c36cf2f
Kevincodexgithub <kevintauro95@gmail.com>
2021-08-23 23:52:47 -0500

    agregar y actualizar registros

--- routes/web.php
+++ routes/web.php
@@ -32,12 +32,17 @@ Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
 // });
 Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
 
+Route::post('cursos', [CursoController::class, 'store'])->name('cursos.store');
+
 // Route::get('cursos/{curso}', function ($curso) {
 //     return "Bienvenido al curso " . $curso;
 // });
 
-Route::get('cursos/{id}', [CursoController::class, 'show'])->name('cursos.show');
+Route::get('cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
 
+Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
+
+Route::put('cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
 /**
  * TODO: El simbolo (?) despues de la variable significa que la variable es opcional al pasarle por la URL
  */
