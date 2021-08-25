39f3955efef931a47bd9df278b671fdae6b04d21
Kevincodexgithub <kevintauro95@gmail.com>
2021-08-24 23:12:28 -0500

    validar formulario

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -9,20 +9,35 @@
         <label>
             Nombre:
             <br>
-            <input type="text" name="name">
+            <input type="text" name="name" value="{{ old('name') }}">
         </label>
+        @error('name')
+            <br>
+            <small>* {{ $message }}</small>
+            <br>
+        @enderror
         <br>
         <label>
             Descripcion:
             <br>
-            <textarea name="descripcion" rows="10"></textarea>
+            <textarea name="descripcion" rows="10">{{ old('descripcion') }}</textarea>
         </label>
+        @error('descripcion')
+            <br>
+            <small>* {{ $message }}</small>
+            <br>
+        @enderror
         <br>
         <label>
             Categoria:
             <br>
-            <input type="text" name="categoria">
+            <input type="text" name="categoria" value="{{ old('categoria') }}">
         </label>
+        @error('categoria')
+            <br>
+            <small>* {{ $message }}</small>
+            <br>
+        @enderror
         <br>
         <button type="submit">enviar</button>
     </form>
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -7,6 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     {{-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> --}}
+    <style>
+        html {
+            filter: invert(100%);
+            color: white;
+            background-color: gray;
+        }
+
+    </style>
     {{-- favicon --}}
     {{-- estilos --}}
 </head>
