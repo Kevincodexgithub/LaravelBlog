c6128edada4546deb0a6233ab9e8851b57d4e63f
Kevincodexgithub <kevintauro95@gmail.com>
2021-08-30 02:00:38 -0500

    crear rutas con route resource

--- routes/web.php
+++ routes/web.php
@@ -25,26 +25,26 @@ Route::get('/', HomeController::class);
 //     return "Bienvenido a la pagina cursos";
 // });
 
-Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
+//Route::get('cursos', [CursoController::class, 'index'])->name('cursos.index');
 
 // Route::get('cursos/create', function () {
 //     return "En esta pagina podras crear un curso";
 // });
-Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
+//Route::get('cursos/create', [CursoController::class, 'create'])->name('cursos.create');
 
-Route::post('cursos', [CursoController::class, 'store'])->name('cursos.store');
+//Route::post('cursos', [CursoController::class, 'store'])->name('cursos.store');
 
 // Route::get('cursos/{curso}', function ($curso) {
 //     return "Bienvenido al curso " . $curso;
 // });
 
-Route::get('cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
+//Route::get('cursos/{curso}', [CursoController::class, 'show'])->name('cursos.show');
 
-Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
+//Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
 
-Route::put('cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
+//Route::put('cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
 
-Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
+//Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
 /**
  * TODO: El simbolo (?) despues de la variable significa que la variable es opcional al pasarle por la URL
  */
@@ -55,3 +55,5 @@ Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('curs
 //         return "Bienvenido al curso " . $curso;
 //     }
 // });
+
+Route::resource('asignaturas', CursoController::class)->parameters(['asignaturas' => 'curso'])->names('cursos');
